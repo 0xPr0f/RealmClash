fea38bd167daeab2d793bc6c593070ef5c6c9c3b
0xPr0f <0xprofoz@gmail.com>
2024-02-14 03:46:45 +0100

    edit

--- webapp/src/app/portfolio/page.js
+++ webapp/src/app/portfolio/page.js
@@ -1,16 +1,14 @@
 import CardBox from "../components/cardBox/cardBox";
 import "./portfolio.css";
-import { useAccount } from "wagmi";
 
 export default function Portfolio() {
-  const { address } = useAccount();
   return (
     <div>
       This is the portfolio page
       <div className="centeredcontainer">
         <div className="centereddiv">
           <span>Your character cards, click to view</span>
-          <div className="grid-container">
+          <div className="gridcontainer">
             <div className="grid">
               <CardBox height={300} width={200} showStats={false} />
               <CardBox height={300} width={200} showStats={false} />
